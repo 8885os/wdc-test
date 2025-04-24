0832270f750905566fe0e7628c54e865cc7cb69d
8885os <d729258@gmail.com>
2025-04-24 20:46:53 +0100

    disabled caching for preview mode

--- src/app/case-studies/[slug]/page.tsx
+++ src/app/case-studies/[slug]/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { urlFor } from '@/lib/utils/image'
 import { CURRENT_STUDY_QUERY } from '@/lib/utils/queries'
 
-export const revalidate = 60
+// Do not export revalidate globally, we'll control it per request.
 
 export async function generateStaticParams() {
 	const caseStudies = await client.fetch<{ slug: string }[]>(
@@ -17,13 +17,15 @@ export async function generateStaticParams() {
 
 const query = CURRENT_STUDY_QUERY
 
-export default async function Page({
-	params,
-}: {
-	params: Promise<{ slug: string }>
-}) {
-	const { slug } = await params
+export async function generateMetadata() {
+	return {
+		// add SEO info if needed
+	}
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
 	const { isEnabled } = await draftMode()
+	const { slug } = params
 
 	const data = await client.fetch(
 		query,
@@ -33,12 +35,23 @@ export default async function Page({
 					perspective: 'previewDrafts',
 					useCdn: false,
 					stega: true,
-				}
-			: undefined
+			  }
+			: {
+					perspective: 'published',
+					useCdn: true,
+			  }
 	)
 
+	// Return headers dynamically to control caching
+	if (!isEnabled) {
+		// Set Cache-Control to revalidate every 60 seconds
+		// Only if the page is in published mode
+		const headers = new Headers()
+		headers.set('Cache-Control', 's-maxage=60, stale-while-revalidate')
+	}
+
 	return (
-		<div className=''>
+		<div>
 			<h1>{data.title}</h1>
 			<div className='h-[200px] w-[300px]'>
 				{data.image ? (
@@ -49,9 +62,7 @@ export default async function Page({
 						alt='Case study thumbnail'
 						style={{ objectFit: 'cover' }}
 					/>
-				) : (
-					''
-				)}
+				) : null}
 			</div>
 		</div>
 	)
